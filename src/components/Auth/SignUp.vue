a71e7c9232863000f4b04d2d5c437dac95edc6bc
christopher4RY <arkarlin486@gmail.com>
2022-11-18 22:36:09 +0630

    sign up & login design css only update

--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -1,21 +1,21 @@
 <template>
   <h1 class="text-primary text-center mt-3">Sign Up</h1>
-  <form>
+  <form @submit.prevent="">
     <div class="form-group">
       <label for="name">Name: </label>
-      <input type="text" class="form-control" />
+      <input type="text" class="form-control" v-model="name" />
     </div>
     <div class="form-group">
       <label for="email">Email: </label>
-      <input type="email" class="form-control" />
+      <input type="email" class="form-control" v-model="email" />
     </div>
     <div class="form-group">
       <label for="password">Password:</label>
-      <input type="password" class="form-control" />
+      <input type="password" class="form-control" v-model="password" />
     </div>
     <div class="form-group">
-      <label for="confirm_password">Confirm Password</label>
-      <input type="password" class="form-control" />
+      <label for="confirm_password">Confirm Password:</label>
+      <input type="password" class="form-control" v-model="confirm_password" />
     </div>
     <button class="btn btn-primary">Sign Up</button>
     <br />
@@ -27,8 +27,17 @@
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
+import { db } from "@/firebase/config";
 export default {
-  setup() {},
+  setup() {
+    let name = ref("");
+    let email = ref("");
+    let password = ref("");
+    let confirm_password = ref("");
+
+    return { name, email, password, confirm_password };
+  },
 };
 </script>
 
